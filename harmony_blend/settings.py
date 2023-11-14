b9f2adfbb4ea4a5ddf30b2207a2a4b96718f8ef3
Robin Muller <robinmullerpython@icloud.com>
2023-11-14 06:32:15 +0100

    add upload photo system

--- harmony_blend/settings.py
+++ harmony_blend/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,6 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils import timezone
+import uuid
 
 
 class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, null=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     presentation = models.TextField(null=True)
     registration_date = models.DateTimeField(default=timezone.now,
                                              verbose_name="Date d'inscription")
